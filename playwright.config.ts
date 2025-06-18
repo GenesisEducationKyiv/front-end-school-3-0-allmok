1b7677b6466328996f20189628869e50a9b10032
Veronika <v.m.pazenko@student.khai.edu>
2025-06-18 23:55:41 +0300

    delete hardcode

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
@@ -9,7 +10,7 @@ export default defineConfig({
   reporter: 'html',
 
   use: {
-    baseURL: 'http://localhost:3000',
+    baseURL: process.env.BASE_URL,
     trace: 'on-first-retry',
   },
 
@@ -22,7 +23,7 @@ export default defineConfig({
 
   webServer: {
     command: 'npm run dev',
-    url: 'http://localhost:3000',
+    url: process.env.BASE_URL,
     reuseExistingServer: !process.env.CI,
     timeout: 120000,
   },

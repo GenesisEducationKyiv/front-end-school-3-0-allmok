56dc6838dcdd483558348855a596ea210d09467a
Veronika <v.m.pazenko@student.khai.edu>
2025-07-06 07:22:44 +0300

    fix config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,32 +1,30 @@
 import 'dotenv/config';
 import { defineConfig, devices } from '@playwright/test';
 
+const baseURL = process.env.BASE_URL;
+
 export default defineConfig({
   testDir: './e2e',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-
   use: {
-    baseURL: process.env.BASE_URL,
+    baseURL,
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
   },
-
   reporter: [['html', { open: 'never' }]],
-
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
   ],
-
   webServer: {
     command: 'npm run dev',
-    url: process.env.BASE_URL,
+    url: baseURL,
     reuseExistingServer: !process.env.CI,
     timeout: 120000,
   },
